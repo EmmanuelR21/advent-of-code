b0e6f48eab9405ef77352022c84cc11ed8ab7f20
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-03-19 10:53:02 -0400

    renamed file

---
+++ src/day1/main.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//53651
+
+int main() {
+  FILE *fptr;
+  fptr = fopen("d1Input", "r");
+  int sum = 0;
+
+  if (fptr != NULL) {
+  }
+
+  printf("Total sum is: %d \n", sum);
+
+  return 0;
+}
